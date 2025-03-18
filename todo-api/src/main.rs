a1a68e33ed8b6fcbba61934dd9b859eec8c3bf54
Boniface <boniface.ebuka@gmail.com>
2025-03-18 21:30:42 +0100

    Added Axum

---
+++ todo-api/src/main.rs
@@ -0,0 +1,8 @@
+use dotenv::dotenv;
+use std::env;
+
+fn main() {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
+    println!("Connecting to: {}", database_url);
+}
